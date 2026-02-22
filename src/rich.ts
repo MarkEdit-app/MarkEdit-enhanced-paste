a3ad1bb28e446f899138a413377019cc4c70d0fe
Ying Zhong <0x00eeee@gmail.com>
2026-02-22 21:40:56 +0800

    Add rich text handling

---
+++ src/rich.ts
@@ -0,0 +1,58 @@
+import TurndownService from 'turndown';
+import { MarkEdit } from 'markedit-api';
+import { turndownOptions } from './settings';
+
+const turndown = new TurndownService({
+  ...{
+    'headingStyle': 'atx',
+    'bulletListMarker': '-',
+    'codeBlockStyle': 'fenced',
+    'emDelimiter': '*',
+    'strongDelimiter': '**',
+  },
+  ...turndownOptions,
+});
+
+export async function handleRichPaste(event: ClipboardEvent, behavior: 'auto' | 'ask') {
+  const html = event.clipboardData?.getData('text/html') ?? '';
+  if (html.length === 0) {
+    return;
+  }
+
+  event.preventDefault();
+  event.stopPropagation();
+
+  if (behavior === 'auto') {
+    pasteAsRichText(html);
+  } else if (behavior === 'ask') {
+    MarkEdit.showContextMenu([
+      {
+        title: 'Paste as Rich Text',
+        action: () => pasteAsRichText(html),
+      },
+      {
+        title: 'Paste as Plain Text',
+        action: () => pasteAsPlainText(),
+      },
+    ]);
+  }
+}
+
+export async function pasteAsPlainText() {
+  const text = await MarkEdit.getPasteboardString();
+  if (text !== undefined) {
+    insertText(text);
+  }
+}
+
+function pasteAsRichText(html: string) {
+  const markdown = turndown.turndown(html);
+  if (markdown.length > 0) {
+    insertText(markdown);
+  }
+}
+
+function insertText(text: string) {
+  const selection = MarkEdit.editorAPI.getSelections()[0];
+  MarkEdit.editorAPI.setText(text, selection);
+}
--- src/settings.ts
+++ src/settings.ts
@@ -2,3 +2,6 @@ import { MarkEdit, JSONObject } from 'markedit-api';
 
 export const rootValue = MarkEdit.userSettings['extension.markeditEnhancedPaste'] as JSONObject ?? {};
 export const enableImagePaste = (rootValue['enableImagePaste'] ?? true) as boolean;
+export const richPasteBehavior = (rootValue['richPasteBehavior'] ?? 'auto') as 'auto' | 'ask' | 'none';
+export const turndownOptions = (rootValue['turndownOptions'] ?? {}) as JSONObject;
+export const pasteAsPlainTextHotKey = (rootValue['pasteAsPlainTextHotKey'] ?? 'Shift-Alt-Mod-v') as string;
